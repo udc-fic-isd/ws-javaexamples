17bba2f7c2239810772c33e63310cfbbc866f3f7
Juan Raposo Santiago <juan.raposo@udc.es>
2019-08-30 11:30:33 +0200

    Añadido constructor privado

--- ws-util/src/main/java/es/udc/ws/util/json/ObjectMapperFactory.java
+++ ws-util/src/main/java/es/udc/ws/util/json/ObjectMapperFactory.java
@@ -5,9 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class ObjectMapperFactory {
     private static ObjectMapper mapper = new ObjectMapper();
     
+    private ObjectMapperFactory() { } 
+    
     public static ObjectMapper instance() { 
         return mapper; 
     }
 
-	
 }
