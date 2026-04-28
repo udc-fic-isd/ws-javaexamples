d1494653f722a01d6bda4bbf4224a3793a711c03
Juan Raposo Santiago <juan.raposo@udc.es>
2026-04-28 20:26:41 +0200

    Cambiado DROP TABLE por DROP TABLE IF EXISTS

--- ws-jdbctutorial/src/sql/MySQLCreateTables.sql
+++ ws-jdbctutorial/src/sql/MySQLCreateTables.sql
@@ -2,7 +2,7 @@
 -- JDBC Tutorial.
 -------------------------------------------------------------------------------
 
-DROP TABLE TutMovie;
+DROP TABLE IF EXISTS TutMovie;
 CREATE TABLE TutMovie ( movieId VARCHAR(40) COLLATE latin1_bin NOT NULL,
     title VARCHAR(255) COLLATE latin1_bin NOT NULL,
     runtime SMALLINT NOT NULL,
